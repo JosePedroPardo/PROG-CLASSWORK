d907159b7af4e079cddfa6f656a411f5b68f4e40
José Pedro Pardo Serrano <josepedropardoserrano@gmail.com>
2023-03-07 10:27:48 +0100

    [Pruebas] pruebas con el hasmap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Map<Integer,String> criptomonedas = new HashMap<>();
+
+        criptomonedas.put(1, "Bitcoin");
+        criptomonedas.put(2, "Etherium");
+        criptomonedas.put(3, "Binance");
+        criptomonedas.put(4, "Cardano");
+        criptomonedas.put(5, "Tether");
+        criptomonedas.put(6, "Estaphium");
+        criptomonedas.put(7, "Estaphirium");
+
+
+        System.out.println(criptomonedas.containsValue("Estaphirium"));
     }
 }
